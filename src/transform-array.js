6d1d16b55246590bdfe80219a804b3e8b071bc16
Spawn <alkspawn@gmail.com>
2020-03-10 22:21:07 +0300

    default

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,31 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+const COMMAND = ['--discard-next','--discard-prev','--double-next','--double-prev'];
+
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw 'Error';
+    };
+    let result = [];
+    let index = 0;
+      while (index < arr.length) {
+      if (arr[index] === COMMAND[0]) {
+        index += 2;
+      } else if (arr[index] ===  COMMAND[1]) {
+        result.pop();
+        index += 1;
+      } else if (arr[index] ===  COMMAND[2]) {
+        if (index + 1 < arr.length) {
+          result.push(arr[index + 1], arr[index + 1]);
+        }
+        index += 2;
+      } else if (arr[index] ===  COMMAND[3]) {
+        if (index > 0) {
+          result.push(arr[index - 1]);
+        }
+        index += 1;
+      } else {
+        result.push(arr[index]);
+        index += 1;
+      }
+    }
+    return result;
+  };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,85 @@
+const   START_CODE_CHAR = 65,
+        LENGTH = 26,
+        CRYPT = /[A-Za-z]/;
+
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    constructor (directCoder = true) {
+        this.direct = directCoder
+    }
+    
+    charToCode(code) {
+        return String.fromCharCode(code + START_CODE_CHAR);
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    codeToChar(char) {
+        return char.toUpperCase().charCodeAt(char) - START_CODE_CHAR;
+    }
+
+    
+    encrypt(string, key) {
+        if (!string || !key) {
+          throw new Error();
+        }
+
+        let keyCount = 0,
+            keyLength = key.length,
+            encryptString = '';
+
+        const encryptChar = (cryptChar) => {
+            if (CRYPT.test(cryptChar)) {
+                encryptString += this.charToCode((this.codeToChar(cryptChar) + this.codeToChar(key[ keyCount++ % keyLength ])) % LENGTH);
+            } else {
+                encryptString += cryptChar;
+            }
+        }
+       
+        if (this.direct) {
+            for (let i = 0, length = string.length; i < length; i++) {
+                let cryptChar = string[i];
+                encryptChar(cryptChar);
+            }
+        } else {
+            for (let i = string.length - 1; i >= 0; i--) {
+                let cryptChar = string[i];
+                encryptChar(cryptChar);
+            }
+        }
+        return encryptString;
+    }
+
+   decrypt(string, key) {
+        if (!string || !key) {
+          throw new Error();
+        }
+
+            let keyCount = 0,
+            keyLength = key.length,
+            decryptString = '';
+
+        const decryptChar = (cryptChar) => {
+            if (CRYPT.test(cryptChar)) {
+                decryptString += this.charToCode((26 + this.codeToChar(cryptChar) - this.codeToChar(key[ keyCount++ % keyLength ])) % LENGTH);
+            } else {
+                decryptString += cryptChar;
+            }
+        }
+
+        if (this.direct) {
+            for (let i = 0, length = string.length; i < length; i++) {
+                let cryptChar = string[i];
+                decryptChar(cryptChar);
+            }
+        } else {
+            for (let i = string.length - 1; i >= 0 ; i--) {
+                let cryptChar = string[i];
+                decryptChar(cryptChar);
+            }
+        }
+
+        return decryptString;
     }
 }
+
 
 module.exports = VigenereCipheringMachine;
